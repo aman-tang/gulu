e1fdb975621a821d611debcbdca36ca26d539ba8
aman-tang <42300236@qq.com>
2019-01-15 16:01:36 +0800

    添加 click 事件

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
-   <button class="g-button" :class="{[`icon-${iconPositioin}`]:true}">
-    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-    <g-icon class="loading" name="loading"></g-icon>
+   <button class="g-button" :class="{[`icon-${iconPositioin}`]:true}" @click="$emit('click')">
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -14,6 +14,10 @@ export default {
   // 下面这种 props 的写法，可解决 class 中有 undefined 的情况
   props: {
     icon: {},
+    loading: {
+      type: Boolean,
+      default: false
+    },
     iconPositioin: {
       type: String,
       default: 'left',
