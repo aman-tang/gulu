3191cce1acd8cf169d696e372393c246bae479d8
aman-tang <42300236@qq.com>
2019-01-17 11:06:56 +0800

    使用 chai.expect 添加四个测试用例，第五个测试要用到 mock，还不会

--- src/app.js
+++ src/app.js
@@ -15,3 +15,98 @@ new Vue({
     loading3: false
   }
 })
+
+import chai from 'chai'
+const expect = chai.expect
+// 单元测试
+{
+  // 动态生成了按钮，通过 JS 把按钮写到了页面
+  // 先把 Button 组件变成了构造函数，然后通过这个构造函数声明一个 vm 实例，再把实例挂载到 #test 上
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  // vm.$mount('#test')
+  vm.$mount()
+  // mount 中也可以什么都没有
+  // 我期待 use 和 href 是 i-settings，因为我设置的 icon 是 settings；
+  // 然后区检查 settings，若我的设置和检查的结果都满足预期，则代码没问题。
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  // 若搞定了，就把该元素从页面删除
+  vm.$el.remove()
+  // 把 vm 对象删掉
+  vm.$destroy()
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  // mock
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function(){
+    console.log(1)
+  })
+  // 测试意图：希望这个函数被执行
+  let button = vm.$el
+  button.click()
+}
