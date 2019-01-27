afefe3fea821bef45b45fe6b4afb63db948a847e
aman-tang <42300236@qq.com>
2019-01-27 20:02:25 +0800

    触发 update:selected 事件的时候添加一个 item 数据

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
+    <div class="line" ref="line"></div>
     <div class="actions-wrapper">
       <slot name="actions"></slot>
     </div>
@@ -13,16 +14,27 @@
     // 访问 eventBus，inject:[] 注入
     inject: ['eventBus'],
     created() {
+      this.eventBus.$on('update:selected', (item, vm) => {
+        console.log(item)
+      })
     }
   }
 </script>
 
 <style scoped lang="scss">
 $tab-height: 40px;
+$blue: blue;
   .tabs-head {
     display: flex;
     height: $tab-height;
     justify-content: flex-start;
+    position: relative;
+    > .line {
+      position: absolute;
+      bottom: 0;
+      border-bottom: 2px solid $blue;
+      width: 5em;
+    }
     > .actions-wrapper {
       margin-left: auto;
     }
