d15547950cd20cd51855b88098dba55abd8a031d
aman-tang <42300236@qq.com>
2019-01-27 17:37:06 +0800

    实现 tab 切换

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
-    <slot name="actions"></slot>
+    <div class="actions-wrapper">
+      <slot name="actions"></slot>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'GuluTabsHead'
+    name: 'GuluTabsHead',
+    // 访问 eventBus，inject:[] 注入
+    inject: ['eventBus'],
+    created() {
+    }
   }
 </script>
 
-<style>
+<style scoped lang="scss">
+$tab-height: 40px;
   .tabs-head {
-
+    display: flex;
+    height: $tab-height;
+    justify-content: flex-start;
+    align-items: center;
+    > .actions-wrapper {
+      margin-left: auto;
+    }
   }
 </style>
